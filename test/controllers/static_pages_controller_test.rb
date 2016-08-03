e708e3a369edea20cd71ec5ba969832e492fda48
Jérôme Labonté <jerome.labonte.2@ulaval.ca>
2016-08-03 02:40:58 +0000

    First static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,18 +2,24 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
+    assert_select "title", "Accueil | Club de boxe les Titans"
+
   end
 
   test "should get schedule" do
     get static_pages_schedule_url
     assert_response :success
+    assert_select "title", "Programmation | Club de boxe les Titans"
+
   end
 
   test "should get contact" do
     get static_pages_contact_url
     assert_response :success
+    assert_select "title", "Contact | Club de boxe les Titans"
+
   end
 
 end
