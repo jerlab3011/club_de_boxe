7fd9cd39dbde70c7773bd515043fa339d7a69e60
Jérôme Labonté <jerome.labonte.2@ulaval.ca>
2016-08-09 02:13:05 +0000

    Attempt to display stats correctly in production

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,7 +11,7 @@ class StaticPagesController < ApplicationController
   def stats
     @users = User.all
     @memberships = Membership.all
-    @active_memberships = Membership.where("end_date > ? AND start_date <= ?", Date.today, Date.today)
+    @active_memberships = Membership.where("end_date > ? AND start_date <= ?", Date.today, Date.today).unscoped
     @active_users = @memberships.map(&:user)
   end
 end
