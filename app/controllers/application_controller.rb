b303d55c1779efa46b5d179291b8fa3dca2edafd
Jérôme Labonté <jerome.labonte.2@ulaval.ca>
2016-08-09 03:44:03 +0000

    Add stats link and translate an error message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
     def logged_in_user
       unless logged_in?
         store_location
-        flash[:danger] = "Please log in."
+        flash[:danger] = "Veuillez svp vous connecter"
         redirect_to connexion_url
       end
     end
